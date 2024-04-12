2b88bf6afa295d2aa73fad76696d87bb31cb0318
Orosten <gabriel.namias@gmail.com>
2024-04-12 17:02:15 +0200

    et bah

--- New Portfolio/JS/Menu.js
+++ New Portfolio/JS/Menu.js
@@ -1,11 +1,8 @@
+
+
 function OpenMenu(){
     let menu = document.querySelector(".menu")
-
-    if (menu.classList.contains("hidden")){
-        menu.classList.remove("hidden")
-    } else if (!menu.classList.contains("hidden")){
-        menu.classList.add("hidden")
-    }
+    menu.classList.toggle('visible');
 }
 
 
