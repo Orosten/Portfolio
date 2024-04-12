d92ab79090da6cef0ca63de52550c9c018803cf3
Orosten <160497630+Orosten@users.noreply.github.com>
2024-04-12 20:07:29 +0200

    ntm les fichier c bon la ?

---
+++ New Portfolio/JS/Menu.js
@@ -0,0 +1,23 @@
+
+
+function OpenMenu(){
+    let menu = document.querySelector(".menu")
+        menu.classList.toggle('hidden');
+        menu.classList.toggle('menuvisible')
+
+    let button = document.querySelector(".menuButton")
+
+   //     if(button.src=('../Asset/menu.png')){
+   //         button.src=('../Asset/croix.png')
+  //          button.classList.add('menuButtonCroix')
+  //      } else if (button.src=('../Asset/croix.png')){
+  //          button.src=('../Asset/menu.png');    
+  //          button.classList.remove('menuButtonCroix')
+ //       }
+    }
+
+
+
+
+
+
