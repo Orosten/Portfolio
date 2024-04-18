16aba1f7b664453b447bb342ccac27d041b4f658
Gabriel Namias <gabriel.namias@gmail.com>
2024-04-18 16:27:05 +0200

    MAJ menu

--- New Portfolio/JS/Menu.js
+++ New Portfolio/JS/Menu.js
@@ -3,18 +3,15 @@
 function OpenMenu(){
     let menu = document.querySelector(".menu")
         menu.classList.toggle('hidden');
-        menu.classList.toggle('menuvisible')
+        menu.classList.toggle('menuvisible');
 
     let button = document.querySelector(".menuButton")
-
-   //     if(button.src=('../Asset/menu.png')){
-   //         button.src=('../Asset/croix.png')
-  //          button.classList.add('menuButtonCroix')
-  //      } else if (button.src=('../Asset/croix.png')){
-  //          button.src=('../Asset/menu.png');    
-  //          button.classList.remove('menuButtonCroix')
- //       }
-    }
+        if(button.src.endsWith('menu.png')){       
+            button.src=('../Asset/croix.png')
+    }   else if (button.src.endsWith('croix.png')){
+            button.src=('../Asset/menu.png');    
+        }   
+}
 
 
 
