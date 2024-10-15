cde37e7c9bb4370316eb31767daf9cf10708d59f
Gabriel Namias <gabriel.namias@gmail.com>
2024-10-15 18:10:27 +0200

    MAJ page entreprise

---
+++ New Portfolio/JS/Entreprise.js
@@ -0,0 +1,21 @@
+function Afficher(x){
+    let icon = x;
+    let infoContent1 = document.querySelector(".infoSpeContent1")
+    let infoContent2 = document.querySelector(".infoSpeContent2")
+    let iconinfo1 = document.querySelector(".infoSpecifiques1")
+    let iconinfo2 = document.querySelector(".infoSpecifiques2")
+
+    if(icon == 1){
+        infoContent1.classList.toggle('hidden');
+        infoContent2.classList.add('hidden');
+        iconinfo1.classList.toggle('infoSpeToggle')
+        iconinfo2.classList.remove('infoSpeToggle')
+    } else if (icon == 2){
+        infoContent2.classList.toggle('hidden');
+        infoContent1.classList.add('hidden');
+        iconinfo2.classList.toggle('infoSpeToggle')
+        iconinfo1.classList.remove('infoSpeToggle')
+    }
+}
+
+
