12f63d972ea3910020632d0ae99e6a923e7c26e8
Orosten <gabriel.namias@gmail.com>
2024-11-21 19:54:40 +0100

    maj realisations

--- New Portfolio/JS/Réalisations.js
+++ New Portfolio/JS/Réalisations.js
@@ -1,14 +1,31 @@
-function button(){
-    let button1 = document.querySelector(".button1")
-        button1.classList.toggle('buttonactif');
-
-    let button2 = document.querySelector(".button2")
-        button2.classList.toggle('buttonactif');
-
+function button(x){
     let formation = document.querySelector(".formation")
-        formation.classList.toggle('hidden')
-
     let entreprise = document.querySelector(".entreprise")
-        entreprise.classList.toggle('hidden')
+    let button1 = document.querySelector(".button1")
+    let button2 = document.querySelector(".button2")
+
+    if (x == 1){
+        if (formation.classList.contains("hidden")){
+            formation.classList.remove("hidden");
+            entreprise.classList.add("hidden");
+            button1.classList.add("active")
+            button2.classList.remove("active")
+        } else {
+            formation.classList.add("hidden");
+            button1.classList.remove("active")
+        }
+    } else if (x == 2){
+        if (entreprise.classList.contains("hidden")){
+            entreprise.classList.remove("hidden");
+            formation.classList.add("hidden");
+            button2.classList.add("active")
+            button1.classList.remove("active")
+        } else {
+            entreprise.classList.add("hidden");
+            button2.classList.remove("active")
+        }
+    } else {
+        alert("no conditions matches")
+    }
 }
 
