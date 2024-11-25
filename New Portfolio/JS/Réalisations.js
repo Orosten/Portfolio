db544afbe6bb2c82c5c5705bd9010df4d65cec60
Orosten <gabriel.namias@gmail.com>
2024-11-25 23:57:03 +0100

    maj réalisations templates finis

--- New Portfolio/JS/Réalisations.js
+++ New Portfolio/JS/Réalisations.js
@@ -29,3 +29,44 @@ function button(x){
     }
 }
 
+function afficherProjet(x){
+
+    let box1 = document.querySelector(".box1")
+    let box2 = document.querySelector(".box2")
+    let box3 = document.querySelector(".box3")
+
+    if(x == 1){
+        box1.classList.remove("hidden");
+        box2.classList.add("hidden");
+        box3.classList.add("hidden");
+    } else if (x == 2){
+        box1.classList.add("hidden");
+        box2.classList.remove("hidden");
+        box3.classList.add("hidden");
+    } else if (x == 3){
+        box1.classList.add("hidden");
+        box2.classList.add("hidden");
+        box3.classList.remove("hidden");
+    } else{
+        alert("No condition matches for box toggle")
+    }
+
+}
+
+function retour(){
+        
+    let box1 = document.querySelector(".box1")
+    let box2 = document.querySelector(".box2")
+    let box3 = document.querySelector(".box3")
+
+    if(!box1.classList.contains("hidden")){
+        box1.classList.add("hidden");
+    } else if(!box2.classList.contains("hidden")){
+        box2.classList.add("hidden");
+    } else if(!box3.classList.contains("hidden")){
+        box3.classList.add("hidden");
+    } else {
+        console.log("pas de hidden a retiré")
+    }
+}
+
